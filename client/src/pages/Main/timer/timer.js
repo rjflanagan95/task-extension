738a3d99350149e037d9216e543cc8719174a786
Robert Flanagan <robertflanagan95@gmail.com>
2019-06-22 03:04:19 -0400

    initial timer working

--- client/src/pages/Main/timer/timer.js
+++ client/src/pages/Main/timer/timer.js
@@ -5,9 +5,65 @@ class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      workInterval: 1,
+      breakInterval: 5,
+      count: 0,
+      timerMinutes: "00",
+      timerSeconds: "00",
+      timerStatus: false
+    }
 
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  startTimer() {
+    clearInterval(this.timer);
+    this.setState({
+      timerStatus: true,
+      count: this.state.workInterval * 60,
+    });
+
+    this.timer = setInterval(this.tick.bind(this), 1000)
+  }
+
+  tick() {
+    let minutesDiv = this.state.count % (60 * 60);
+    let minutes = Math.floor(minutesDiv / 60);
+    if (minutes < 10) { minutes = "0" + minutes }
+    let secondsDiv = minutesDiv % (60);
+    let seconds = Math.ceil(secondsDiv);
+    if (seconds < 10) { seconds = "0" + seconds }
+    if (seconds === 0) { seconds = "00" }
+    console.log(seconds);
+
+    this.setState({
+      count: this.state.count - 1,
+      timerMinutes: minutes,
+      timerSeconds: seconds
+    });
+
+    if (this.state.count === 0) {
+      clearInterval(this.timer);
+      
+      this.setState({
+        timerStatus: false,
+        timerMinutes: "00",
+        timerSeconds: "00"
+      })
     }
   }
+
+  stopTimer() {
+    clearInterval(this.timer);
+    this.setState({
+      count: 0,
+      timerMinutes: "00",
+      timerSeconds: "00",
+      timerStatus: false
+    })
+  }
+
 
   render() {
 
@@ -15,6 +71,9 @@ class Timer extends Component {
       <div className="panel timerPanel">
         <div className="panelBody timerBody">
           <img alt="pomodoro timer" className="timerImg" src={tomatoImg}/>
+          <div>{this.state.timerMinutes}:{this.state.timerSeconds}</div>
+          <button onClick={this.startTimer}>Start Timer</button>
+          <button onClick={this.stopTimer}>Stop Timer</button>
         </div>
       </div>
     );
