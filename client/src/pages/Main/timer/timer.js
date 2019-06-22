72601842c19417dbb7208f45153d3fcbb60224e1
Robert Flanagan <robertflanagan95@gmail.com>
2019-06-22 04:46:54 -0400

    timer cycle functions working

--- client/src/pages/Main/timer/timer.js
+++ client/src/pages/Main/timer/timer.js
@@ -7,66 +7,132 @@ class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      workInterval: 1,
-      breakInterval: 5,
+      workInterval: 0.1,
+      workCycles: 4,
+      breakCycles: 3,
       count: 0,
       timerMinutes: "00",
       timerSeconds: "00",
       timerStatus: false,
       showTomato: true,
-      pomodoroStatus: "working..."
+      pomodoroStatus: "..."
     }
 
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
     this.showTimer = this.showTimer.bind(this);
+    this.workCycle = this.workCycle.bind(this);
+    this.breakCycle = this.breakCycle.bind(this);
   }
 
+  // start the timer after the start button is clicked
   startTimer() {
-    clearInterval(this.timer);
+    clearInterval(this.workTimer);
+    clearInterval(this.breakTimer);
+
     this.setState({
-      timerStatus: true,
-      count: this.state.workInterval * 60,
+      timerStatus: true
     });
 
-    this.timer = setInterval(this.tick.bind(this), 1000)
+    // begin the first work cycle
+    this.workCycle();
   }
 
+  // counts seconds remaining on the clock
   tick() {
-    let minutesDiv = this.state.count % (60 * 60);
+    // parsing minutes and seconds
+    let newCount = this.state.count - 1;
+    let minutesDiv = newCount % (60 * 60);
     let minutes = Math.floor(minutesDiv / 60);
-    if (minutes < 10) { minutes = "0" + minutes }
     let secondsDiv = minutesDiv % (60);
     let seconds = Math.ceil(secondsDiv);
+
+    // formatting for display
+    if (minutes < 10) { minutes = "0" + minutes }
     if (seconds < 10) { seconds = "0" + seconds }
     if (seconds === 0) { seconds = "00" }
-    console.log(seconds);
 
+    // update clock
     this.setState({
-      count: this.state.count - 1,
+      count: newCount,
       timerMinutes: minutes,
       timerSeconds: seconds
     });
 
+    // if the timer is up... otherwise the timer will proceed to the next tick
     if (this.state.count === 0) {
-      clearInterval(this.timer);
+      if (this.state.pomodoroStatus === "working...") {
+        clearInterval(this.workTimer);
+      } else if (this.state.pomodoroStatus === "on a break...") {
+        clearInterval(this.breakTimer);
+      }
 
       this.setState({
         timerStatus: false,
         timerMinutes: "00",
         timerSeconds: "00"
-      })
+      });
+
+      this.cycleCheck();
     }
   }
 
   stopTimer() {
-    clearInterval(this.timer);
+    clearInterval(this.workTimer);
+    clearInterval(this.breakTimer);
+
     this.setState({
       count: 0,
       timerMinutes: "00",
       timerSeconds: "00",
-      timerStatus: false
+      timerStatus: false,
+      workCycles: 4,
+      breakCycles: 3,
+      pomodoroStatus: "..."
     })
+  }
+
+  workCycle() {
+    this.setState({
+      pomodoroStatus: "working...",
+      count: this.state.workInterval * 60
+    });
+
+    this.workTimer = setInterval(this.tick.bind(this), 1000);
+  }
+
+  breakCycle() {
+    this.setState({
+      pomodoroStatus: "on a break...",
+      count: this.state.breakInterval * 60
+    });
+
+    this.breakTimer = setInterval(this.tick.bind(this), 1000);
+  }
+
+  cycleCheck() {
+    if (this.state.pomodoroStatus === "working...") {
+      clearInterval(this.workTimer);
+      this.setState({
+        workCycles: this.state.workCycles - 1
+      });
+
+      // if the number of work cycles are over, stop the timer
+      if (this.state.workCycles === 0) {
+        this.stopTimer()
+        this.setState({
+          showTomato: true
+        });
+        // otherwise proceed to the next break cycle
+      } else {
+        this.breakCycle();
+      }
+
+    } else if (this.state.pomodoroStatus === "on a break...") {
+      clearInterval(this.breakTimer);
+
+      this.workCycle();
+    }
   }
 
   showTimer() {
